3141ca9ba6d3c4a186deb1bfffd5bb75c4902726
iamvedantp <vedantp183@gmail.com>
2024-02-27 19:11:23 +0530

    Values extracted

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -12,9 +13,17 @@ class NewItem extends StatefulWidget {
 
 class _NewItemState extends State<NewItem> {
 final _formKey = GlobalKey<FormState>();
+var _enteredName = '';
+var _enteredQuantity = 1;
+var _selectedCategory = categories [Categories.vegetables]!;
 
 void _saveItem () {
-  _formKey.currentState!.validate();
+ if( _formKey.currentState!.validate()){
+_formKey.currentState!.save();
+ print(_enteredName);
+ print(_enteredQuantity);
+ print(_selectedCategory);
+ }
 }
 
   @override
@@ -43,6 +52,9 @@ void _saveItem () {
                   }
                   return null;
                 },
+                onSaved: (value) {
+                 _enteredName = value!;
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -53,7 +65,7 @@ void _saveItem () {
                         label: Text('Quantity'),
                       ),
                       keyboardType: TextInputType.number,
-                      initialValue: '1',
+                      initialValue: _enteredQuantity.toString(),
                        validator: (value) {
                   if (value == null ||
                   value.isEmpty ||
@@ -63,11 +75,15 @@ void _saveItem () {
                   }
                   return null;
                 },
+                onSaved: (value) {
+                  _enteredQuantity = int.parse(value!);
+                },
                     ),
                   ),
                   const SizedBox(width: 8),
                   Expanded(
                     child: DropdownButtonFormField(
+                      value: _selectedCategory,
                       items: [
                       for (final category in categories.entries)
                         DropdownMenuItem(
@@ -84,7 +100,14 @@ void _saveItem () {
                             ],
                           ),
                         ),
-                    ], onChanged: (value) {}),
+                    ],
+                     onChanged: (value) {
+                      setState(() {
+                      _selectedCategory = value!;  
+                      });
+                      
+
+                    }),
                   )
                 ],
               ),
