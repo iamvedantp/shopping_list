e67ba0fe55bb46247c4ee81f4fec2e1b680fe304
iamvedantp <vedantp183@gmail.com>
2025-02-14 22:52:02 +0530

    Initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:shopping_list/widgets/grocery_list.dart';
 
 void main() {
@@ -13,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Groceries',
       theme: ThemeData.dark().copyWith(
         useMaterial3: true,
